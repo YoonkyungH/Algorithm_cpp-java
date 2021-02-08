e02b675e46ec558257937f21844df48f0bc1b49b
yoonkyungH <ykha9911@gmail.com>
2021-02-08 15:16:23 +0900

    Practice

---
+++ 00.Practice/b3046.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  ios::sync_with_stdio(0);
+
+  int R1, S;
+  cin >> R1 >> S;
+
+  cout << (2*S)-R1 << '\n';
+
+  return 0;
+}
---
+++ 00.Practice/b9654.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  // string first[5] = {"N2 Bomber", "J-Type 327", "NX Cruiser", "N1 Starfighter", "Royal Cruiser"};
+  // string second[5] = {""};
+  printf("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\n");
+  printf("N2 Bomber      Heavy Fighter  Limited    21        \n");
+  printf("J-Type 327     Light Combat   Unlimited  1        \n");
+  printf("NX Cruiser     Medium Fighter Limited    18        \n");
+  printf("N1 Starfighter Medium Fighter Unlimited  25        \n");
+  printf("Royal Cruiser  Light Combat   Limited    4         \n");
+
+
+  return 0;
+}
