3a86e1823791ea074b6275bc936aaaae47468e87
YoonkyungH <ykha9911@gmail.com>
2021-06-24 12:11:30 +0900

    base3

---
+++ 00.Programmers/03_base3.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+
+#include <iostream>
+
+using namespace std;
+
+int solution(int n) {
+    int answer = 0;
+    vector<int> v;
+
+    while(n != 0) {
+        v.push_back(n%3);
+        n /= 3;
+    }
+
+    // for(int i=0; i<v.size(); i++) {
+    //     cout << v[i] << ' ';
+    // }
+
+    reverse(v.begin(), v.end());
+    for(int i=0; i<v.size(); i++) {
+        answer += v[i] * pow(3, i);
+    }
+
+    return answer;
+}
