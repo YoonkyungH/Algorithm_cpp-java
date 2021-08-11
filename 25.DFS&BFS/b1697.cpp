4bd79f88ec21f69d2a00a1c1a1b03bc963f84c71
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-08-11 23:45:47 +0900

    디버깅 중

---
+++ 25.DFS&BFS/b1697.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+#define MAX 100000
+
+// 숨바꼭질
+
+queue<pair<int, int> > q;
+bool visited[MAX] = {false, };
+int N, K;
+
+int bfs(int n) {
+    q.push(make_pair(n, 0));
+
+    visited[n] = true;
+
+    while(!q.empty()) {
+        int x = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
+
+        if(x == K) { return cnt; }
+
+        if(x+1<MAX && visited[x+1]==false) {
+            q.push(make_pair(x+1, cnt+1));
+            visited[x+1] = true;
+        }
+        if(x-1<MAX && visited[x-1]==false) {
+            q.push(make_pair(x-1, cnt+1));
+            visited[x-1] = true;
+        }
+        if(x*2<MAX && visited[x*2]==false) {
+            q.push(make_pair(x*2, cnt+1));
+            visited[2*x] = true;
+        }
+
+    }
+
+    return 0;
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+
+    cin >> N >> K;
+
+    cout << bfs(N) << '\n';
+
+
+
+    return 0;
+}
