bb520ed82443ab78d39d1ad332ef4b320303e55d
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-08-27 00:05:22 +0900

    Sort

--- 26.Sort/b16112.cpp
+++ 26.Sort/b16112.cpp
@@ -8,36 +8,27 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
 
-    int n, k;       // n: 퀘스트 k: 최대 활성 스톤 개수
-    int sum = 0;    // 경험치 합
+    long long n, k;       // n: 퀘스트 k: 최대 활성 스톤 개수
+    long long sum = 0;    // 경험치 합
     cin >> n >> k;
 
     if(n == 1) {
         cout << "0\n"; return 0;
     }
 
-    vector<int> v;
+    vector<int> v(n);
     for(int i=0; i<n; i++) {
-        int q;
-        cin >> q;
+        // int q;
+        cin >> v[i];
 
-        v.push_back(q);
+        // v.push_back(q);
     }
 
     sort(v.begin(), v.end());
 
-    sum = v[1];
-    int kNum;   // 지금 활성화 되어있는 스톤의 개수
-    if(k == 1) { kNum = 1; }
-    else { kNum = 2; }
-    for(int i=2; i<n; i++) {
-        sum += v[i] * kNum;
-        // for(int j=0; j<kNum; j++) {
-        //     sum += v[i];
-        // }
-    
-        if(kNum < k) { kNum++; }
-
+    for(long long i=1; i<n; i++) {
+        // k가 최대 활성 스톤이므로 min
+        sum += v[i] * min(i, k);
     }
 
     cout << sum << '\n';
