1354a635169df57a380f4c3508ff587d33560e91
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-09-04 15:02:36 +0900

    자물쇠와 열쇠

--- 00.Programmers/study05.cpp
+++ 00.Programmers/study05.cpp
@@ -1,69 +1,81 @@
 // 자물쇠와 열쇠
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>    // copy 함수
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
 using namespace std;
 
-int keySize;
-int lockSize;
-int boardSize;
+// 90도 시계방향 회전 
 
-vector<vector<int>> rotation(vector<vector<int>> key) {   // 시계방향 90도 회전 구현
-    vector<vector<int> > tmp(keySize, vector<int>(keySize));           // 회전한 값을 arr에 넣기 위한 임시 2차원 배열
+// 회전한 키를 백터 형태로 반환
 
-    // copy(&key[0][0], &key[0][0] + keySize * keySize, &tmp[0][0]);
+vector<vector<int>> rotation (vector<vector<int>> key, int keySize) {
+    vector<vector<int>> newKey(keySize, vector<int>(keySize));
+    // keySize 개의 원소에 각 원소는 vector<int> keysize만큼 초기화
 
     for(int i=0; i<keySize; i++) {
         for(int j=0; j<keySize; j++) {
-            tmp[i][j] = key[keySize - j - 1][i];
+            newKey[i][j] = key[keySize - j - 1][i];
         }
     }
+
+    return newKey; 
 }
 
-// 지금 키가 있는 자리가 자물쇠와 풀리도록 일치하는지 확인하는 함수
-bool check(int x, int y, vector<vector<int>> key, vector<vector<int>> board) {
+bool check(int x, int y, int boardSize, int keySize, vector<vector<int>> key, vector<vector<int>> board) {
     for(int i=x; i<x+keySize; i++) {
         for(int j=y; j<y+keySize; j++) {
-            if(i>=keySize-1 && i<=boardSize-keySize && j>=keySize-1 && j<=boardSize-keySize) {
-                if (key[i-x][i-y] + board[i][j] != 1) {
-                    return false;   // 튀어나온데 또 튀어나오거나 비었는데 채우지 못했거나
-                }
-            }
             
+            board[i][j] += key[i-x][j-y];
         }
     }
     
+    for(int i=keySize-1; i<=boardSize-keySize; i++) {
+        for(int j=keySize-1; j<=boardSize-keySize; j++) {
+            if(board[i][j] != 1) { return false; }
+        }
+    }
+
     return true;
 }
 
 bool solution(vector<vector<int>> key, vector<vector<int>> lock) {
     bool answer = false;
 
-    keySize = key.size();
-    lockSize = lock.size();
-    boardSize = lockSize + (keySize-1) * 2;
+    int keySize = key.size();
+    int lockSize = lock.size();
+    int boardSize = lockSize + (keySize-1) * 2;
 
+    // 키를 자물쇠에 딱 맞춰서 비교해야 하는 것이 아니기 때문에 훨씬 큰 보드를 벡터로 구현
+    // 보드 사이즈는 lockSize + (keySize-1)*2
+    // 자물쇠에 하나라도 걸쳐야 하기 때문에 -1을 해주고 위아래를 생각하면 *2 해주는 것이 맞음
     vector<vector<int> > board(boardSize, vector<int> (boardSize));
-    
-    // 큰 보드에 자물쇠 (lock) 옮기기
+
+    // 보드에 정 가운데 자물쇠 옮기기
     for(int i=keySize-1; i<=boardSize-keySize; i++) {
         for(int j=keySize-1; j<=boardSize-keySize; j++) {
             board[i][j] = lock[i-keySize+1][j-keySize+1];
         }
     }
 
-    // 다 돌아보기
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<=(boardSize - keySize); j++) {
-            for(int k=0; k<=(boardSize - keySize); k++) {
-                if(check(j, k, key, board)) { answer = true; return answer; }
+    // 탐색
+    // 방향을 확인하는 코드가 제일 바깥 for문이어야 함
+    for(int r=0; r<4; r++) {    // 네 방향 돌려서 확인 (ratation)
+    
+        for(int i=0; i<=boardSize-keySize; i++) {   // 보드를 옮겨다니면서
+            for(int j=0; j<=boardSize-keySize; j++) {
+                if(check(i, j, boardSize, keySize, key, board)) {
+                    answer = true; return answer;
+                }
+
             }
         }
 
-        key = rotation(key);
+        key = rotation(key, keySize);
     }
-    
+
+
+
     return answer;
 }
