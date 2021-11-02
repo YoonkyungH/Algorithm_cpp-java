cc3c81bfb28da4dd37b4ccfb4a339f2de8ea4c1c
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2021-11-02 12:02:20 +0900

    사탕 게임

--- 31./b3085.cpp
+++ 31./b3085.cpp
@@ -5,29 +5,41 @@
 
 using namespace std;
 
-char tmp;
 char board[50][50];
+int N;
 
-bool checkRow(int i, int j, char c) {   // 수평
-    if(board[i-1][j] == tmp || board[i+1][j] == tmp) {
-        return true;
-    } 
+int check() {
+    int cnt = 1; int tmp = 0;
 
-    return false;
+    // 가로
+    for(int i=0; i<N; i++) {
+        for(int j=1; j<N; j++) {
+            if(board[i][j] == board[i][j-1]) { cnt++; }
+            else { cnt = 1; }
+
+            tmp = max(tmp, cnt);
+        }
+        cnt = 1;
+    }
+
+    // 세로
+    cnt = 1;
+    for(int j=0; j<N; j++) {
+        for(int i=1; i<N; i++) {
+            if(board[i][j] == board[i-1][j]) { cnt++; }
+            else { cnt = 1; }
+
+            tmp = max(tmp, cnt);
+        }
+        cnt = 1;
+    }
+
+    return tmp;
 }
 
-bool checkCol(int i, int j, char c) {   // 세로
-    if(board[i][j-1] == tmp || board[i][j+1] == tmp) {
-        return true;
-    } 
-
-    return false;
-}
-
-int main() {
+int main () {
     ios::sync_with_stdio(0);
 
-    int N;  // 보드의 크기
     cin >> N;
 
     for(int i=0; i<N; i++) {
@@ -36,133 +48,27 @@ int main() {
         }
     }
 
-    tmp = board[0][0];
     int ans = 0;
-    int cnt = 1;
-    bool flag = false;
-    
-    // 가로
     for(int i=0; i<N; i++) {
         for(int j=0; j<N; j++) {
-            if(i == 0 && j == 0) { continue; }
+            if(j+1 < N) {    // 가로, 범위 벗어나지 않으면
+                swap(board[i][j], board[i][j+1]);
 
-            if(flag == true) {
-                tmp = board[i][j]; cnt = 1; flag = false;
-            } else {
-                if(board[i][j] == tmp) {
-                    cnt++;
-                    ans = max(cnt, ans);
-                    cout << "==tmp\n";
-                    cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-                    continue;
-                    }
-                else {
-                    if(checkRow(i, j, tmp)) {
-                        cnt++;
-                        ans = max(cnt, ans);
-                        cout << "check\n";
-                        cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-                        // flag = true;
-                    }
-                }   
+                // int cnt = check();
+                ans = max(ans, check());
+
+                swap(board[i][j], board[i][j+1]);   // 원상복구
             }
 
-            
+            if(i+1 < N) {    // 가로, 범위 벗어나지 않으면
+                swap(board[i][j], board[i+1][j]);
 
-            // if(board[i][j] == tmp) {
-            //     cnt++;
-            //     if(i == N-1) { cnt++; ans = max(cnt, ans); }
-            //     if(checkRow(i+1, j+1, tmp)) {
-            //         cnt++;
-            //         ans = max(cnt, ans);
-            //         cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-            //     }
-            // }
-            // else {  // 다른 문자가 나왔다면
-            //     tmp = board[i][j];
-            //     cnt = 1;
-            //     continue;
-            // }
+                ans = max(ans, check());
 
-        }
-        flag = true;
-        // tmp = board[i+1][0]; cnt = 1;
-    }
-
-
-    // 세로
-    tmp = board[0][0];
-    for(int i=0; i<N; i++) {
-        for(int j=0; j<N; j++) {
-            if(i == 0 && j == 0) { continue; }
-
-            if(flag == true) {
-                tmp = board[j][i]; cnt = 1; flag = false;
-            } else {
-                if(board[j][i] == tmp) {
-                    cnt++;
-                    ans = max(cnt, ans);
-                    cout << "==tmp\n";
-                    cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-                    continue;
-                }
-                else {
-                    if(checkRow(j, i, tmp)) {
-                        cnt++;
-                        ans = max(cnt, ans);
-                        cout << "check\n";
-                        cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-                        // flag = true;
-                    }
-                }   
+                swap(board[i][j], board[i+1][j]);   // 원상복구
             }
-
-            
-
-            // if(board[i][j] == tmp) {
-            //     cnt++;
-            //     if(i == N-1) { cnt++; ans = max(cnt, ans); }
-            //     if(checkRow(i+1, j+1, tmp)) {
-            //         cnt++;
-            //         ans = max(cnt, ans);
-            //         cout << "ROW count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-            //     }
-            // }
-            // else {  // 다른 문자가 나왔다면
-            //     tmp = board[i][j];
-            //     cnt = 1;
-            //     continue;
-            // }
-
         }
-        flag = true;
-        // tmp = board[i+1][0]; cnt = 1;
     }
-
-    // 세로
-    // tmp = board[0][0];
-    // for(int i=0; i<N; i++) {
-    //     for(int j=0; j<N; j++) {
-    //         if(i == 0 && j == 0) { continue; }
-
-    //         if(board[j][i] == tmp) {
-    //             cnt++;
-    //             if(j == N-1) { cnt++; ans = max(cnt, ans); }
-    //             if(checkCol(j+1, i+1, tmp)) {
-    //                 cnt++;
-    //                 ans = max(cnt, ans);
-    //                 cout << "COL count: " << cnt << " i: " << i << " j: " << j << " ans= " << ans << '\n';
-    //             }
-    //         }
-    //         else {  // 다른 문자가 나왔다면
-    //             tmp = board[i][j];
-    //             continue;
-    //         }
-
-    //     }
-    //     tmp = board[0][i+1];
-    //     cnt = 0;
-    // }
 
     cout << ans << '\n';
 
