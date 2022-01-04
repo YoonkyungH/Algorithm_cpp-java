19158d0a423fff1202261c51d1c294cb20fa943d
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-01-04 13:37:25 +0900

    피보나치 수2

---
+++ SDS알고리즘특강/2일차/b2748.cpp
@@ -0,0 +1,26 @@
+// 피보나치 수2
+// 시간 복잡도
+
+#include <iostream>
+
+using namespace std;
+
+long long arr[91];
+
+int main() {
+    ios::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+
+    arr[0] = 0;
+    arr[1] = 1;
+
+    for(int i=2; i<=n; i++) {
+        arr[i] = arr[i-1] + arr[i-2];
+    }
+
+    cout << arr[n] << '\n';
+
+    return 0;
+}
