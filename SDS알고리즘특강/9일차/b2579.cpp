340651ff653da80e2e5486a786006a5d7c13957e
윤경 <57247474+YoonkyungH@users.noreply.github.com>
2022-01-16 17:33:20 +0900

    계단 오르기 (DP)

--- SDS알고리즘특강/9일차/b2579.cpp
+++ SDS알고리즘특강/9일차/b2579.cpp
@@ -22,6 +22,10 @@ int main() {
     dp[1] = stair[1];
     dp[2] = max(stair[2], stair[1]+stair[2]);
     dp[3] = max(stair[1]+stair[3], stair[2]+stair[3]);
+    // 아니면 위의 코드와 반복문 안의 코드 대신 주석코드끼리 짜도 가능함
+    // dp[0] = 0;
+    // dp[1] = stair[1];
+    // dp[2] = stair[1] + stair[2];
 
     for(int i=3; i<=n; i++) {
         // 3번 연속 한 계단씩 오르는 경우를 체크하지 않는 이유는
@@ -29,6 +33,8 @@ int main() {
         // 두 칸 뛰어서 오는게 큰지, 두 칸 뛴 다음 한 칸 뛰는게 나은지 검사하기 때문에 
         // 연속 세 칸을 뛰는 경우는 애초에 배제된다.
         dp[i] = max(stair[i] + dp[i-2], stair[i] + stair[i-1] + dp[i-3]);
+
+        // dp[i] = max(dp[i - 3] + stair[i - 1], dp[i - 2]) + stair[i];
     }
 
     cout << dp[n] << '\n';
